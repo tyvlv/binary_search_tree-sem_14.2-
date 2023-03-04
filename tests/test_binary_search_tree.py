8e9ce10fcb857be2db12041b7adf3b10269f48a4
tychkov.vv <tyvl@yandex.ru>
2023-03-04 23:01:35 +0300

    Add tests for BST. 100% coverage.

--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -30,3 +30,17 @@ def test_bst_insert_right(bst):
     """Добавляем первый элемент справа."""
     bst.insert({'id': 50})
     assert bst.root.right.data == {'id': 50}
+
+
+def test_bst_insert_left_left(bst):
+    """Добавляем два элемента слева."""
+    bst.insert({'id': 30})
+    bst.insert({'id': 25})
+    assert bst.root.left.left.data == {'id': 25}
+
+
+def test_bst_insert_right_right(bst):
+    """Добавляем два элемента справа."""
+    bst.insert({'id': 50})
+    bst.insert({'id': 60})
+    assert bst.root.right.right.data == {'id': 60}
